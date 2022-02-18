97050f139dfd33688e52722e50c0e103b4657a2e
Tattybubu Talantbekova <tattybubu.talantbekova.kk@gmail.com>
2022-02-18 21:01:19 +0600

    finished!

---
+++ task-8-loan-calc/js/app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#loan-form");
+form.addEventListener('submit', calculateResults);
+
+function calculateResults(e) {
+    const amount = document.querySelector("#amount");
+    const interest = document.querySelector("#interest");
+    const years = document.querySelector("#years");
+
+    const monthlyPayment = document.querySelector('#monthly-payment');
+    const totalPayment = document.querySelector('#total-payment');
+    const totalInterest = document.querySelector('#total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x-1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * colculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * colculatedPayments) - principal).toFixed(2);
+    } else {
+        alert('Please fill in the fields!');
+    }
+
+    e.preventDefault();
+}
